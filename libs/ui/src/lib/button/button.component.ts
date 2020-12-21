d18f593ddccc8cbb886e3fe8ec3b18488453499f
shuufei <digital.shuufei@gmail.com>
2020-12-21 21:59:24 +0900

    change prefix of ui lib

--- libs/ui/src/lib/button/button.component.ts
+++ libs/ui/src/lib/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-  selector: 'wishlist-button',
+  selector: 'ui-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
