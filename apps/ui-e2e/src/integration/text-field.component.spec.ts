79a7fc67996fc4fffd70d474347f5daa82ac6385
shuufei <digital.shuufei@gmail.com>
2020-12-27 11:29:16 +0900

    change text-field e2e scenario

--- apps/ui-e2e/src/integration/text-field.component.spec.ts
+++ apps/ui-e2e/src/integration/text-field.component.spec.ts
@@ -9,6 +9,14 @@ describe('text-field', () => {
 
     it('typing', () => {
       cy.get('.ui-body').first().type('ui-body input');
+      cy.get('.ui-display1').type('ui-display1 input');
+      cy.get('.ui-heading').type('ui-heading input');
+      cy.get('.ui-heading-b').type('ui-heading-b input');
+      cy.get('.ui-subheading').type('ui-subheading input');
+      cy.get('.ui-subheading-b').type('ui-subheading-b input');
+      cy.get('.ui-body').last().type('ui-body input');
+      cy.get('.ui-body-b').type('ui-body-b input');
+      cy.get('.ui-caption').type('ui-caption input');
       cy.matchImageSnapshot(`${componentName}--input--typing`);
     });
   });
@@ -22,6 +30,14 @@ describe('text-field', () => {
 
     it('typing', () => {
       cy.get('.ui-body').first().type('ui-body textarea');
+      cy.get('.ui-display1').type('ui-display1 textarea');
+      cy.get('.ui-heading').type('ui-heading textarea');
+      cy.get('.ui-heading-b').type('ui-heading-b textarea');
+      cy.get('.ui-subheading').type('ui-subheading textarea');
+      cy.get('.ui-subheading-b').type('ui-subheading-b textarea');
+      cy.get('.ui-body').last().type('ui-body textarea');
+      cy.get('.ui-body-b').type('ui-body-b textarea');
+      cy.get('.ui-caption').type('ui-caption textarea');
       cy.matchImageSnapshot(`${componentName}--textarea--typing`);
     });
   });
