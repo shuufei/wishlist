e5571ad8d1e5c602c9a20a12fc62b4b22d85fe9b
shuufei <digital.shuufei@gmail.com>
2020-12-30 16:30:31 +0900

    change config file for added lib

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  projects: ['<rootDir>/apps/wishlist-desktop-app', '<rootDir>/libs/ui'],
+  projects: [
+    '<rootDir>/apps/wishlist-desktop-app',
+    '<rootDir>/libs/ui',
+    '<rootDir>/libs/mylist/shared/data-access',
+    '<rootDir>/libs/mylist/feature-shell',
+    '<rootDir>/libs/mylist/domain',
+    '<rootDir>/libs/shared/domain',
+  ],
 };
