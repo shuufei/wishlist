247bb5c0926b7fd8aef5d5e19cba81ef173bc39a
shuufei <digital.shuufei@gmail.com>
2021-01-30 18:10:42 +0900

    change val name

--- libs/shared/ui/src/lib/sandbox/form/form.component.spec.ts
+++ libs/shared/ui/src/lib/sandbox/form/form.component.spec.ts
@@ -26,21 +26,21 @@ fdescribe('FormComponent', () => {
   });
 
   it('input要素に値を入力した時、入力した値がタイトルに表示される', () => {
-    const inputValue = 'form test';
+    const value = 'form test';
 
     // input要素にテキストを入力する
     const input = fixture.debugElement.query(By.css('input'));
     const inputEl: HTMLInputElement = input.nativeElement;
-    inputEl.value = inputValue;
+    inputEl.value = value;
     inputEl.dispatchEvent(new Event('input'));
 
     fixture.detectChanges();
 
     // タイトル要素を取得
-    const titleValue = fixture.debugElement.query(By.css('h1'));
-    const titleValueEl: HTMLElement = titleValue.nativeElement;
+    const title = fixture.debugElement.query(By.css('h1'));
+    const titleEl: HTMLElement = title.nativeElement;
 
     // タイトル要素のテキストが、入力値であることを確認
-    expect(titleValueEl.textContent).toBe(inputValue);
+    expect(titleEl.textContent).toBe(value);
   });
 });
